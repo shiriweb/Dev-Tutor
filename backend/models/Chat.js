671fd4bb832d606cc8a46db4dbc8663dbd755c7e
shiriweb <shirisha.singh36@gmail.com>
2025-11-01 18:00:52 +0545

    feat: added frontend

--- backend/models/Chat.js
+++ backend/models/Chat.js
@@ -1,11 +1,44 @@
 import mongoose from "mongoose";
-const chatSchema = new mongoose.Schema({
-    userId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required:true
-    },
-    
+
+const messageSchema = new mongoose.Schema({
+  sender: {
+    type: String,
+    enum: ["user", "assistant"],
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-export const Chat = new mongoose.model("Chat", chatSchema);
+const chatSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  topic: {
+    type: String,
+    required: true,
+  },
+  message: [messageSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+chatSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+export const Chat = mongoose.model("Chat", chatSchema);
